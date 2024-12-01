362049dd4cd7a23dcea9e956eb568a7f25645ae6
Zishan Ahmed <zishan.ahmed1210@gmail.com>
2024-12-02 02:31:12 +0600

    ios firebase another attempt

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,34 @@
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import './firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import './utility/theme.dart';
 import './routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyDTX8feb7JciLRcMpzISJDUjRWRo57XxkY",
-        authDomain: "feedback-work-61234.firebaseapp.com",
-        databaseURL: "https://feedback-work-61234-default-rtdb.firebaseio.com",
-        projectId: "feedback-work-61234",
-        storageBucket: "feedback-work-61234.appspot.com",
-        messagingSenderId: "583751085628",
-        appId: "1:583751085628:web:363db814f62e525f50dd39",
-        measurementId: "G-18GL8LD6DE",
-      ),
-    );
-  } else {
-    await Firebase.initializeApp();
-  }
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // if (kIsWeb) {
+  //   await Firebase.initializeApp(
+  //     options: const FirebaseOptions(
+  //       apiKey: "AIzaSyDTX8feb7JciLRcMpzISJDUjRWRo57XxkY",
+  //       authDomain: "feedback-work-61234.firebaseapp.com",
+  //       databaseURL: "https://feedback-work-61234-default-rtdb.firebaseio.com",
+  //       projectId: "feedback-work-61234",
+  //       storageBucket: "feedback-work-61234.appspot.com",
+  //       messagingSenderId: "583751085628",
+  //       appId: "1:583751085628:web:363db814f62e525f50dd39",
+  //       measurementId: "G-18GL8LD6DE",
+  //     ),
+  //   );
+  // } else {
+  //   await Firebase.initializeApp(
+  //     options: DefaultFirebaseOptions.currentPlatform,
+  //   );
+  // }
   runApp(const ProviderScope(child: MyApp()));
 }
 
